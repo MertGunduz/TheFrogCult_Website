3d048fd1b1ebb154c91a5910dac80e13b739c0e2
Mert Gunduz <merttgg@gmail.com>
2021-12-22 04:24:43 +0300

    Desktop Music Player Developed

--- Scripts/indexscript.js
+++ Scripts/indexscript.js
@@ -11,8 +11,11 @@ const indexHeaderPart = document.querySelector(".index-header")
 const indexMainPart = document.querySelector(".index-main");
 const informationFooterPart = document.querySelector(".information-footer");
 
-// Fixed Song Button
+// Fixed Song Button & Audio Stuff
+var i = 0;
 const songButton = document.querySelector(".music-play-div");
+const musicToPlay = new Audio("Audio/TheFrogCult_Music.wav");
+const musicPlayerImage = document.querySelector("#music-player-img");
 
 openModalButton.addEventListener("click", function() {
       modalBackground.style.visibility = "visible";
@@ -32,4 +35,19 @@ closeModalButton.addEventListener("click", function() {
       indexHeaderPart.style.filter = "blur(0px)";
       indexMainPart.style.filter = "blur(0px)";
       informationFooterPart.style.filter = "blur(0px)";
+});
+
+songButton.addEventListener("click", function() {
+      if (i % 2 == 0)
+      {
+            musicPlayerImage.src = "Images/Icons/TheFrogCult_StopIcon.svg";
+            musicToPlay.play();
+      }
+      else 
+      {
+            musicPlayerImage.src = "Images/Icons/TheFrogCult_StartIcon.svg";
+            musicToPlay.pause();
+      }
+      
+      i++;
 });
